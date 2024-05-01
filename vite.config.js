7f755a22033405f896f1ac5741a4aa3e1b7842c6
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-05-01 17:35:07 -0500

    se agrega archivo robots a build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
+import { robots } from 'vite-plugin-robots'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    robots({
+      /* pass your config */
+    })
   ],
+
 })
