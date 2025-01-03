87ae768dfa9fa7ff38c9a819fc69377f7b1dab6f
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-01-03 17:15:31 -0500

    Changed icons

--- src/components/icons/ComponentCV.jsx
+++ src/components/icons/ComponentCV.jsx
@@ -1,12 +1,24 @@
 export const ComponentCV = () => {
-    return (
-        <svg
-            width="800px"
-            height="800px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M16 11L12 15M12 15L8 11M12 15V3M21 15V17C21 18.1046 20.1046 19 19 19H5C3.89543 19 3 18.1046 3 17V15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-    )
+  return (
+    // <svg
+    //     width="800px"
+    //     height="800px"
+    //     viewBox="0 0 24 24"
+    //     fill="none"
+    //     xmlns="http://www.w3.org/2000/svg">
+    //     <path d="M16 11L12 15M12 15L8 11M12 15V3M21 15V17C21 18.1046 20.1046 19 19 19H5C3.89543 19 3 18.1046 3 17V15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    // </svg>
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='448'
+      height='512'
+      viewBox='35 35 448 512'
+    >
+      <path
+        fill='currentColor'
+        transform='scale(1.14)'
+        d='M48 32C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm98.88 133.234c19.636 0 37.082 6.789 49.929 16.971c11.88 9.452 17.444 18.907 22.298 27.393l-33.923 16.949c-2.427-5.565-5.347-11.387-12.846-17.682c-8.248-6.552-16.478-8.484-23.524-8.484c-27.626 0-42.17 25.693-42.17 54.287c0 37.573 19.161 56.22 42.17 56.22c22.3 0 31.278-15.51 37.08-25.435L219.6 302.66c-6.315 9.926-12.374 19.635-25.95 29.069c-7.262 5.09-23.977 15.037-47.736 15.037C100.586 346.766 64 313.81 64 255.87c0-50.636 34.415-90.637 82.88-90.637m75.483 5.328h45.565L303.31 292.24l35.125-121.678H384l-59.379 171.112H281.01z'
+      />
+    </svg>
+  )
 }
