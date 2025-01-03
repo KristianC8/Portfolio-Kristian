61ec1fb4322644e6cfb8b8ce3904d3c27d821bc8
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-01-03 17:14:56 -0500

    Changed year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,23 @@ import './Footer.css'
 import { ComponentGithub } from './icons/ComponentGithub'
 
 export const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className='footer-logo' title='Developed by Kristian Cifuentes'></div>
-            <div className='footer-date'>2024</div>
-            <a href="https://github.com/KristianC8/Portfolio-Kristian" target='_blank' rel='noopener noreferer'>
-                Code
-                <ComponentGithub />
-            </a>
-        </footer>
-    )
+  const date = new Date()
+  const year = date.getFullYear()
+  return (
+    <footer className='footer'>
+      <div
+        className='footer-logo'
+        title='Developed by Kristian Cifuentes'
+      ></div>
+      <div className='footer-date'>{year}</div>
+      <a
+        href='https://github.com/KristianC8/Portfolio-Kristian'
+        target='_blank'
+        rel='noopener noreferer'
+      >
+        Code
+        <ComponentGithub />
+      </a>
+    </footer>
+  )
 }
